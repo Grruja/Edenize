a0fb0dfaefb7c3a0bb864d8fde712a91bc38e695
AleexxG <alexanderg.sln@gmail.com>
2024-01-04 01:23:33 +0100

    Displayed products in cards & made them responsive

--- view/index.php
+++ view/index.php
@@ -11,6 +11,23 @@
         <main>
             <div class="container">
                 <h1>Welcome!</h1>
+
+                <div class="row row-cols-1 row-cols-md-4 row-cols-sm-2 g-4">
+                    <?php foreach ($products as $item) { ?>
+                        <div class="col custom-col-xs-2">
+                            <div id="cardContainer" class="bg-light rounded-2 shadow p-4 h-100">
+                                <h4 class="fw-bold"><?= $item['name'] ?></h4>
+                                <p><?= $item['quantity'] ?> left</p>
+                                <div id="cardPriceContainer" class="d-flex justify-content-between">
+                                    <p class="fs-5 fw-bold">$<?= $item['price'] ?></p>
+                                    <a href="index.php" class="btn btn-success d-flex align-items-center">
+                                        <i class="fa-solid fa-cart-arrow-down"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </main>
     <?php include 'components/footer.php'; ?>
