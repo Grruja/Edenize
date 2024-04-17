6489768a92f1b2e47b1dd953fbc609a2f4252830
AleexxG <alexanderg.sln@gmail.com>
2024-04-17 02:00:50 +0200

    Favicon & thumbnail added

--- view/components/head.php
+++ view/components/head.php
@@ -10,7 +10,16 @@ global $publicPath;
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <meta property="og:url" content="http://website.com/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Edenize - All Plants You Need">
+        <meta property="og:description" content="Bring a touch of nature into your home with our beautiful collection of plants.">
+        <meta property="og:image" content="https://i.ibb.co/5GfPHbN/thumbnail.jpg">
+        <link rel="icon" type="image/png" href="<?= $publicPath ?>/favicons/favicon.ico"/>
+        
         <title>Edenize</title>
+        <meta content="Bring a touch of nature into your home with our beautiful collection of plants." name="description">
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/51cad990b5.js" crossorigin="anonymous"></script>
